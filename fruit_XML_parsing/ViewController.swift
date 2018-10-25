42180824ba57038cd871797fc2c8d91ed232c7bd
D7703_24 <d7703_24@d7703-24ui-imac.local>
2018-10-25 14:53:31 +0900

    중간고사

--- fruit_XML_parsing/ViewController.swift
+++ fruit_XML_parsing/ViewController.swift
@@ -14,19 +14,15 @@ class ViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, 
     var item:[String:String] = [:]  // item[key] => value
     var elements:[[String:String]] = []
     var currentElement = ""
-
-//    @IBOutlet weak var fName: UILabel!
-//    @IBOutlet weak var fColor: UILabel!
-//    @IBOutlet weak var fCost: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myTableView.delegate = self
         myTableView.dataSource = self
+        self.title = "나의 책장"
         
-        if let path = Bundle.main.url(forResource: "fruit", withExtension: "xml") {
-            //print(path)
+        if let path = Bundle.main.url(forResource: "book", withExtension: "xml") {
             if let parser = XMLParser(contentsOf: path) {
                 parser.delegate = self
 
@@ -41,21 +37,6 @@ class ViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, 
             print("xml file not found")
         }
         
-//        let strURL = "http://api.androidhive.info/pizza/?format=xml"
-//
-//        if NSURL(string: strURL) != nil {
-//            if let parser = XMLParser(contentsOf: NSURL(string: strURL)! as URL) {
-//                parser.delegate = self
-//
-//                if parser.parse() {
-//                    print("parsing success")
-//                    print(elements)
-//                } else {
-//                    print("parsing fail")
-//                }
-//
-//            }
-//        }
     }
 
     // UITableView Delegate
@@ -65,15 +46,14 @@ class ViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
-        
         let myItem = elements[indexPath.row]
         
-        let name = cell.viewWithTag(1) as! UILabel
-        let color = cell.viewWithTag(2) as! UILabel
+        let title = cell.viewWithTag(1) as! UILabel
+        let author = cell.viewWithTag(2) as! UILabel
 
         
-        name.text = myItem["title"]
-        color.text = myItem["author"]
+        title.text = myItem["title"]
+        author.text = myItem["author"]
 
         
         return cell
@@ -85,27 +65,20 @@ class ViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, 
         currentElement = elementName
         
         print("currentElement = \(elementName)")
-        // 결과를 저장할 자료구조 메모리 할당
-        
-        // tag 이름이 elements이거나 item이면 초기화
-//        if elementName == "elements" {
-//            elements = []
-//        } else if elementName == "item" {
-//            item = [:]
-//        }
+
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
             let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
         print("data = \(data)")
         if !data.isEmpty {
-            item[currentElement] = data  // item[key] => value  =>  [key:value]
+            item[currentElement] = data
         }
     }
 
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         if elementName == "book" {
-            elements.append(item) //
+            elements.append(item)
         }
     }
 }
